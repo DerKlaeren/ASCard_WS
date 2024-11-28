0156a0312280eab3b1a546794abff7668d4ff5d1
WarWolfen <warwolfen@gmail.com>
2024-11-28 23:48:25 +0100

    Added swagger

--- app.js
+++ app.js
@@ -187,6 +187,43 @@ function get_contacts(conn) {
 //	res.send('Hello World! Aber sowas von! yeah AGAIN!');
 //});
 
+
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
+
 app.listen(port, () => {
   console.log(`Web service listening at http://localhost:${port}`);
 });
