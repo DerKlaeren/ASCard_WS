010276a452ae651ce7ec9a3a35a2c00c42a3ff0f
WarWolfen <warwolfen@gmail.com>
2025-01-21 15:47:54 +0100

    Added static html main page in views Added static css file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -15,22 +16,12 @@ logger.info('Starting up ASCard Webservice...');
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
 app.use(bodyParser.urlencoded({ extended: true, }));
 app.use(bodyParser.json());
-
-var homepage = '';
-homepage = homepage + '<html>';
-homepage = homepage + '    <body>';
-homepage = homepage + '        <h1>ASCard WS</h1>';
-homepage = homepage + '        <ul>';
-homepage = homepage + '            <li><a href="https://www.ascard.net/app/login.php" target="_BLANK">ASCard-App</a></li>';
-homepage = homepage + '            <li><a href="https://ws.ascard.net/api-docs" target="_BLANK">api-docs</a></li>';
-homepage = homepage + '            <li><a href="https://ws.ascard.net/log/consolelog.txt" target="_BLANK">consolelog.txt</a> (synched every 2 minutes, passenger.log)</li>';
-homepage = homepage + '        </ul>';
-homepage = homepage + '    </body>';
-homepage = homepage + '</html>';
-homepage = homepage + '';
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', (req, res) => { res.send(homepage); });
 app.use("/games", require("./routes/games"));
+//app.use("/players", require("./routes/players"));
+
+app.use('/', (req, res) => { res.sendFile(path.join(__dirname, "views", "home.html")); });
 
 app.listen(port, () => { logger.info(`Web service listening at http://localhost:${port}`); });
