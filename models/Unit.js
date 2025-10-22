a6fe336dade361aac33bd22c99484fe09d5c1a07
Fussvolk <werner.kewenig@arcor.de>
2025-10-22 20:34:29 +0200

    Change statuses to status

--- models/Unit.js
+++ models/Unit.js
@@ -11,14 +11,14 @@ class Unit {
         this.type = data.as_tp;
         this.pilot = new Pilot(data); // 1zu1
         this.formation = new Formation(data);
-        this.unit_statuses = []; //1zu1
+        this.unit_status = []; //1zu1
     }
 
     addStatus(row) {
         // nur hinzufügen, wenn tatsächlich Statusdaten vorhanden sind
         if (row.armor !== null || row.heat !== null) {
             const status = new UnitStatus(row);
-            this.unit_statuses.push(status);
+            this.unit_status.push(status);
         }
     }
 }
