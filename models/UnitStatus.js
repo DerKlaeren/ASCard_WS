a76bf2030fa81a90c251b90dfcd476cf30d22a52
Fussvolk <werner.kewenig@arcor.de>
2025-10-22 21:00:11 +0200

    Test

--- models/UnitStatus.js
+++ models/UnitStatus.js
@@ -5,8 +5,15 @@ class UnitStatus {
         this.armor = data.armor;
         this.unit_status = data.unit_status;
         this.round = data.round;
+        this.structure = data.structure;
+        this.active_narc = data.active_narc;
+        this.active_tag = data.active_tag;
+        this.active_water = data.active_water;
+        this.active_routed = data.active_routed;
+        this.active_bid = data.active_bid;
+        this.unit_statusimageurl = data.unit_statusimageurl;
     }
 
 }
 
-module.exports = UnitStatus;              |
+module.exports = UnitStatus;
